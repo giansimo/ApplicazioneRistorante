573105312a30bbae3e283241cb63a8e4297d0988
emanuelromeo <emanuel.romeo@hotmail.com>
2025-01-21 18:33:56 +0100

    Added InputHandler, DataValidator, MenuConsoleHandler, and updated PortataFactory

--- src/PortataFactory.java
+++ src/PortataFactory.java
@@ -1,94 +1,40 @@
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.*;
 
+//Rimuovere scanner, da inserire in una classe a parte
 public class PortataFactory {
-    public static Portata getPortata(TipologiaPortata tipologiaPortata, Scanner scanner) {
-        System.out.print("Nome: ");
-        String nome = scanner.nextLine().trim();
-        System.out.print("Descrizione: ");
-        String descrizione = scanner.nextLine().trim();
-        System.out.print("Ingredienti (separati da una virgola, es. \"latte, farina, zucchero\"): ");
-        String[] ingredienti = scanner.nextLine().replaceAll(" ", "").split(",");
-        System.out.print("Prezzo: ");
-        double prezzo;
-        while (true) {
-            try {
-                prezzo = scanner.nextDouble();
-                break;
-            } catch (NoSuchElementException | IllegalStateException e) {
-                System.out.println("\u001B[31m" + e.getMessage() + "\u001B[0m");
-                System.out.print("Inserisci un prezzo valido: ");
-                scanner.nextLine();
+    public static Portata getPortata(Map<String, String> map) {
+
+        String nome = map.get("nome");
+        String descrizione = map.get("descrizione");
+        double prezzo = Double.parseDouble(map.get("prezzo"));
+        String[] ingredienti = map.get("ingredienti").replaceAll(" ", "").split(",");
+
+        switch (TipologiaPortata.valueOf(map.get("tipo"))) {
+            case BEVANDE -> {
+                Temperatura temperatura = Temperatura.valueOf(map.get("temperatura"));
+                boolean alcolico = !map.get("alcolico").equalsIgnoreCase("no");
+                return new Bevande(nome, descrizione, prezzo, ingredienti, temperatura, alcolico);
             }
-        }
-        scanner.nextLine();
-        switch (tipologiaPortata) {
 
             case ANTIPASTI -> {
-                System.out.print("Antipasto vegano (sì/no)? ");
-                String veganoAnswer = scanner.nextLine().trim();
-                while (!veganoAnswer.equalsIgnoreCase("sì") && !veganoAnswer.equalsIgnoreCase("si") && !veganoAnswer.equalsIgnoreCase("no")) {
-                    System.out.println("\u001B[31m" + "Input non valido" + "\u001B[0m");
-                    System.out.print("Inserisci \"sì\" o \"no\": ");
-                    veganoAnswer = scanner.nextLine().trim();
-                }
-                boolean isVegano = !veganoAnswer.equalsIgnoreCase("no");
+                boolean isVegano = !map.get("isVegano").equalsIgnoreCase("no");
                 return new Antipasti(nome, descrizione, prezzo, ingredienti, isVegano);
             }
 
             case PRIMI -> {
-                System.out.print("Tipo di cottura: ");
-                String tipoDiCottura = scanner.nextLine().trim();
+                String tipoDiCottura = map.get("tipoDiCottura");
                 return new Primi(nome, descrizione, prezzo, ingredienti, tipoDiCottura);
             }
 
             case SECONDI -> {
-                System.out.print("Cottura: ");
-                String cottura = scanner.nextLine().trim();
-                System.out.print("Categoria alimentazione: ");
-                String categoriaAlimentazione = scanner.nextLine().trim();
+                String cottura = map.get("cottura");
+                String categoriaAlimentazione = map.get("categoriaAlimentazione");
                 return new Secondi(nome, descrizione, prezzo, ingredienti, cottura, categoriaAlimentazione);
             }
 
             case DESSERT -> {
-                System.out.print("Temperatura (\"caldo\", \"tiepido\", \"freddo\"): ");
-                String temperaturaAnswer;
-                while (true) {
-                    try {
-                        temperaturaAnswer = scanner.nextLine().trim().toUpperCase();
-                        Temperatura temperatura = Temperatura.valueOf(temperaturaAnswer);
-                        return new Dessert(nome, descrizione, prezzo, ingredienti, temperatura);
-                    } catch (IllegalArgumentException e) {
-                        System.out.println("\u001B[31m" + e.getMessage() + "\u001B[0m");
-                        System.out.print("Inserisci una temperatura valida: ");
-                    }
-                }
-            }
-
-            case BEVANDE -> {
-                System.out.print("Temperatura (\"caldo\", \"tiepido\", \"freddo\"): ");
-                String temperaturaAnswer;
-                Temperatura temperatura;
-                while (true) {
-                    try {
-                        temperaturaAnswer = scanner.nextLine().trim().toUpperCase();
-                        temperatura = Temperatura.valueOf(temperaturaAnswer);
-                        break;
-                    } catch (IllegalArgumentException e) {
-                        System.out.println("\u001B[31m" + e.getMessage() + "\u001B[0m");
-                        System.out.print("Inserisci una temperatura valida: ");
-                    }
-                }
-                System.out.print("Bevanda alcolica (sì/no)? ");
-                String alcolicoAnswer = scanner.nextLine().trim();
-                while (!alcolicoAnswer.equalsIgnoreCase("sì") && !alcolicoAnswer.equalsIgnoreCase("si") && !alcolicoAnswer.equalsIgnoreCase("no")) {
-                    System.out.println("\u001B[31m" + "Input non valido" + "\u001B[0m");
-                    System.out.print("Inserisci \"sì\" o \"no\": ");
-                    alcolicoAnswer = scanner.nextLine().trim();
-                }
-                boolean alcolico = !alcolicoAnswer.equalsIgnoreCase("no");
-                return new Bevande(nome, descrizione, prezzo, ingredienti, temperatura, alcolico);
+                Temperatura temperatura = Temperatura.valueOf(map.get("temperatura"));
+                return new Dessert(nome, descrizione, prezzo, ingredienti, temperatura);
             }
 
             default -> {
